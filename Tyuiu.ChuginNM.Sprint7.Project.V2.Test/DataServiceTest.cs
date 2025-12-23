cfc1c7728a0e675f5a9a57045da74c108ab20f4d
CDOOR <CDOOR@CHUGIN>
2025-12-24 01:00:07 +0500

    .

---
+++ Tyuiu.ChuginNM.Sprint7.Project.V2.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ChuginNM.Sprint7.Project.V2.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint7.Project.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+
+        }
+    }
+}
